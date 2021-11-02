e8cb2f29ec44db6d825780e9a41ce7eac18000d4
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-11-02 10:48:54 -0400

    criando o layout e algumas funcoes em js

---
+++ calculadora.js
@@ -0,0 +1,32 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        inicia() {
+            this.cliqueBotoes();
+        },
+        cliqueBotoes() {
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+            });
+        },
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        },
+    };
+};
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
